e059440f5038db69be6b19a8d5f3f04a055b30a5
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-07 12:02:35 -0500

    corrección sintaxis

--- src/view/signUp.js
+++ src/view/signUp.js
@@ -1,13 +1,13 @@
 export default () => {
   const templateSignUp = document.createElement('section');
-  const viewSignUp = `<div class="Registro-vent">
+  const viewSignUp = `<div>
   <a href=#/>◀</a>
   <h2>Regístrate</h2>
   <input id="name" type="text" placeholder="Nombre Completo" required>
   <input id="username" type="text" placeholder="Nombre de usuario" required>
   <input id="email" type="email" placeholder="ichef@example.com" required>
   <p id= "errorMail" style="display: none"> Correo inválido. Por favor, revísalo nuevamente.</p>
-  <input id="password" type="password" placeholder="***" required>
+  < input id ="password" type ="password" placeholder = "*********" required >
   <button id="btn-google">Google</button>
   <button id="btn-correo">Registrarse</button>
   </div>`;
--- src/view/signin.js
+++ src/view/signin.js
@@ -5,6 +5,7 @@ export default () => {
       <h1> Inicia Sesión </h1><br />
       <input type="email" placeholder="usuario@example.com"><br />
       <input type="password" placeholder="********"><br />
+       <button>Google</button><br />
       <button>Inicia Sesión</button><br />`;
 
   templateSignIn.classList.add('position');

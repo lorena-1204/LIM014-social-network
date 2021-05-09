ce48e98aae40a1af001044e2195a098e08476391
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-08 21:53:49 -0500

    añadir template initalPage & depurar esLint

--- src/view/signUp.js
+++ src/view/signUp.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import { registerNewUser, registerWithGoogle } from '../lib/view-controller.js';
 
 export default () => {
--- src/view/signin.js
+++ src/view/signin.js
@@ -1,15 +1,23 @@
+// eslint-disable-next-line import/no-cycle
+import { signInWithEmail } from '../lib/view-controller.js';
+
 export default () => {
   const templateSignIn = document.createElement('section');
   const viewSignIn = `
       <a href=#/>◀</a>
       <h1> Inicia Sesión </h1><br />
-      <input type="email" placeholder="usuario@example.com"><br />
-      <input type="password" placeholder="********"><br />
-      <button>Google</button><br />
-      <button>Inicia Sesión</button><br />`;
+      <input id="email" type="email" placeholder="usuario@example.com"><br />
+      <input id="password" type="password" placeholder="********"><br />
+      <p id= "errorMail" style="display: none"> Correo inválido. Por favor, revísalo nuevamente.</p>
+      <button id="btn-google">Google</button><br />
+      <button id="btn-correo">Inicia Sesión</button><br />`;
 
   templateSignIn.classList.add('position');
   templateSignIn.innerHTML = viewSignIn;
 
+  const btnRegister = templateSignIn.querySelector('#btn-correo');
+  btnRegister.addEventListener('click', () => {
+    signInWithEmail();
+  });
   return templateSignIn;
 };

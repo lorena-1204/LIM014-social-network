8912e8e5f9d6b890c8a9981500925aac5dbb3fec
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-08 21:50:28 -0500

    funciones para iniciar sesión

--- src/lib/view-controller.js
+++ src/lib/view-controller.js
@@ -1,23 +1,36 @@
 // Llamamos las funciones del FireBase con .then && Catch
-import { register, registerGoogle } from './firebase-controller.js';
+import { register, registerGoogle, signInEmail } from './firebase-controller.js';
+// eslint-disable-next-line import/no-cycle
+import { changeHash } from '../view-controls/index.js';
 
 export const registerNewUser = () => {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#password').value;
   register(email, password)
     .then(() => {
-      console.log('Se registro correctamente');
+      changeHash('/Initialpage');
     })
     .catch(() => {
       document.getElementById('errorMail').style.display = 'block';
     });
 };
-
 export const registerWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   registerGoogle(provider)
     .then(() => {
-      console.log('Se registro correctamente');
+      changeHash('/Initialpage');
+    })
+    .catch(() => {
+      document.getElementById('errorMail').style.display = 'block';
+    });
+};
+// Iniciar Sesión
+export const signInWithEmail = () => {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  signInEmail(email, password)
+    .then(() => {
+      changeHash('/Initialpage');
     })
     .catch(() => {
       document.getElementById('errorMail').style.display = 'block';

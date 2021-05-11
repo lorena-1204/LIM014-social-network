3ec71dcdeb9d6c8d4d5a6c37bd427aef052b2d9b
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-10 22:38:11 -0500

    modularizas funciones auth & firestore

--- src/lib/firebase-controller.js
+++ src/lib/firebase-controller.js
@@ -1,13 +1,15 @@
+// eslint-disable-next-line import/no-cycle
+import { auth } from '../main.js';
 // Funciones Firebase
 
 // registro con email
 // eslint-disable-next-line max-len
-export const register = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password);
+export const register = (email, password) => auth.createUserWithEmailAndPassword(email, password);
 
 // registro con google
 // eslint-disable-next-line max-len
-export const registerGoogle = (provider) => firebase.auth().signInWithPopup(provider);
+export const registerGoogle = (provider) => auth.signInWithPopup(provider);
 
 // ingreso con email
 // eslint-disable-next-line max-len
-export const signInEmail = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
+export const signInEmail = (email, password) => auth.signInWithEmailAndPassword(email, password);

6bb2f5422343cc43ffaff3459103bda20955263a
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-14 10:59:34 -0500

    modularización del templatePost y función ordenPostData

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,28 +1,22 @@
 // eslint-disable-next-line import/no-cycle
-import { deletePost } from './firestore-controller.js';
+import { deletePost, orderPostbyTimeDesc } from './firestore-controller.js';
+import { templatePost } from './templates-sections.js';
 
-const setupPosts = (data) => {
+export const idDocumentPost = (e) => {
+  const idPost = e.target.dataset.id;
+  deletePost(idPost);
+};
+export const setupPosts = (data) => {
   const postList = document.querySelector('.posts');
   if (data.length) {
-    let html = '';
+    let container = '';
     data.forEach((doc) => {
-      const li = `
-          <section style="background-color:skyblue;">
-          <h5 id ="ruta">${doc.email}</h5><span>${doc.id}</span>
-          <p>${doc.post}</p>
-          <button id=${doc.id} class="btnDelete" data-id=${doc.id}> eliminar </button>
-          <button id=${doc.id} class="btnEdit" data-id=${doc.id}> editar </button>
-          </section>
-      `;
-      html += li;
+      container += templatePost(doc);
     });
-    postList.innerHTML = html;
-    const h5 = document.querySelectorAll('.btnDelete');
-    h5.forEach((element) => {
-      element.addEventListener('click', (event) => {
-        const id = event.target.dataset.id;
-        deletePost(id).then(() => console.log('eliminadooooo'));
-      });
+    postList.innerHTML = container;
+    const btnDeletePost = document.querySelectorAll('.btn-delete');
+    btnDeletePost.forEach((element) => {
+      element.addEventListener('click', idDocumentPost);
     });
   } else {
     postList.innerHTML = '<h4 class="text-white">Login to See Posts</h4>';
@@ -31,12 +25,7 @@ const setupPosts = (data) => {
 export const showPost = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      /* firebase.firestore().collection('posts').orderBy('timePost', 'desc')
-        .get()
-        .then((snapshot) => {
-          setupPosts(snapshot.docs);
-        }); */
-      firebase.firestore().collection('posts').orderBy('timePost', 'desc')
+      orderPostbyTimeDesc()
         .onSnapshot((querySnapshot) => {
           const output = [];
           querySnapshot.forEach((doc) => {
@@ -45,7 +34,6 @@ export const showPost = () => {
           setupPosts(output);
         });
     } else {
-      console.log('signout');
       setupPosts([]);
     }
   });

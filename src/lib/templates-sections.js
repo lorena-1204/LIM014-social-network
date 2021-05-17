852632e8e5e560d1e1f61cad75cd4c932d457b92
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-16 22:58:46 -0500

    añadir funciones para concatenar nuevos id's // añadir nuevos atributos para la identificación de los post

--- src/lib/templates-sections.js
+++ src/lib/templates-sections.js
@@ -1,10 +1,10 @@
 export const templatePost = (doc) => {
   const template = `
-    <section style="background-color:skyblue;">
-    <h5 id ="ruta">${doc.email}</h5>
-    <p>${doc.post}</p>
-    <button id=${doc.id} class="btn-delete" data-id=${doc.id}> eliminar </button>
-    <button id=${doc.id} class="btn-edit" data-id=${doc.id}> editar </button>
+    <section style="background-color:skyblue;" id=${doc.id}-post>
+    <h5 id="ruta" data-id=${doc.id}>${doc.email}</h5>
+    <p id=${doc.id}-text-post>${doc.post}</p>
+    <button id=${doc.id}-btn-delete class="btn-delete" data-id=${doc.id}> eliminar </button>
+    <button id=${doc.idUser}-btn-edit class="btn-edit" data-id=${doc.id}> editar </button>
     </section>
     `;
   return template;
@@ -12,6 +12,14 @@ export const templatePost = (doc) => {
 export const forEachBtnDelete = (data, callback) => {
   data.forEach((e) => {
     e.addEventListener('click', callback);
-    console.log('delete');
   });
+};
+export const concatId = (textPostOrigin, suffixTextPost) => {
+  const idText = '#';
+  const idTextOriginString = idText + textPostOrigin + suffixTextPost;
+  return idTextOriginString;
+};
+export const createId = (textPostOrigin, suffixTextPost) => {
+  const idTextOriginString = textPostOrigin + suffixTextPost;
+  return idTextOriginString;
 };

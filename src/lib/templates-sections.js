cf5f27ce68acb3c0f073c17943eb832bc1d2ab0e
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-18 01:50:52 -0500

    mejorando manipulación del DOM y modularizando la función de crear botones

--- src/lib/templates-sections.js
+++ src/lib/templates-sections.js
@@ -3,10 +3,15 @@ export const templatePost = (doc) => {
   section.style = 'background-color:skyblue';
   const template = `
     <h5 id ="ruta">${doc.email}</h5>
-    <p>${doc.post}</p>
-    <button id=${doc.id} class="btn-delete" data-id=${doc.id}> eliminar </button>
-    <button id=${doc.id} class="btn-edit" data-id=${doc.id}> editar </button>
+    <p id ="text-post">${doc.post}</p>
     `;
   section.innerHTML = template;
   return section;
+};
+export const createAttributesButton = (textContentButton, classNameButton, docId) => {
+  const btnPost = document.createElement('button');
+  btnPost.textContent = textContentButton;
+  btnPost.classList = classNameButton;
+  btnPost.dataset.id = docId;
+  return btnPost;
 };

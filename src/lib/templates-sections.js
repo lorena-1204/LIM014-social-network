7aeec97c84ec2107c023acd02e9b31dd29abd3cf
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-21 13:21:14 -0500

    mejoras de código style

--- src/lib/templates-sections.js
+++ src/lib/templates-sections.js
@@ -1,14 +1,13 @@
 export const templatePost = (doc) => {
   const section = document.createElement('section');
-  section.style = 'background-color:skyblue';
+  section.classList.add('templatePost');
   const template = `
     <h5 id ="ruta">${doc.email}</h5>
     <p id ="text-post">${doc.post}</p>
     <button class="like__btn">
       <span id="icon"><i class="far fa-thumbs-up"></i></span>
       <span id="count">0</span> Like
-    </button>
-    `;
+    </button> `;
   section.innerHTML = template;
   return section;
 };

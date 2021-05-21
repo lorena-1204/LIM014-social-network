61b7000eab8394fb5d8541c8e2ae01a13054b8c6
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-05-20 23:31:27 -0500

    eliminando función like

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -59,21 +59,6 @@ export const setupPosts = (data, user, templateInitialPage) => {
       }
     });
     // añadir botón like
-    const likeBtn = document.querySelector('.like__btn');
-    const likeIcon = document.querySelector('#icon');
-    const count = document.querySelector('#count');
-    let clicked = false;
-    likeBtn.addEventListener('click', () => {
-      if (!clicked) {
-        clicked = true;
-        likeIcon.innerHTML = '<i class="fas fa-thumbs-up"></i>';
-        count.textContent++;
-      } else {
-        clicked = false;
-        likeIcon.innerHTML = '<i class="far fa-thumbs-up"></i>';
-        count.textContent--;
-      }
-    });
   } else {
     postList.innerHTML = '<h4 class="text-white">Login to See Posts</h4>';
   }

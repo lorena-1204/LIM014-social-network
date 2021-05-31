e18da185b9d005f7f2da1f1cff26c2c319b09197
lorena-1204 <mlrc1204@gmail.com>
2021-05-31 08:37:20 -0500

    test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,33 @@
-// import { register, registerWithGoogle, signInEmail} from '../src/lib/firebase-controller';
+// import {
+//   register,
+// //   registerGoogle,
+// //   signInEmail,
+// //   currentUser,
+// //   sendEmailVerification,
+// //   signOut,
+// } from '../src/lib/firebase-controller';
 
-// // configurando firebase mock
-// const firebasemock = require('firebase-mock');
+// configurando firebase mock
+const firebasemock = require('firebase-mock');
 
-// const mockauth = new firebasemock.MockAuthentication();
+const mockauth = new firebasemock.MockAuthentication();
 
-// mockauth.autoFlush();
+mockauth.autoFlush();
 
-// global.firebase = firebasemock.MockFirebaseSdk(
-//   // use null if your code does not use RTDB
-//   () => null,
-//   () => mockauth,
+global.firebase = firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  () => null,
+  () => mockauth,
+);
 
-// );
+// register, registerGoogle, signInEmail, currentUser, sendEmailVerification, signOut,
+
+// describe('register', () => {
+//   it('I should log in', (done) => {
+//     register('hola@gmail.com', '123456').then((user) => {
+//       expect(user.email).toBe('hola@gmail.com');
+//       //   expect(userLog.isAnonymous).toBe(false);
+//       done();
+//     });
+//   });
+// });

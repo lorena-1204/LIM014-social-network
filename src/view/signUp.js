01973d0781d425e55e57e83fef93c5dc9d55a727
lorena-1204 <mlrc1204@gmail.com>
2021-05-30 21:47:52 -0500

    back

--- src/view/signUp.js
+++ src/view/signUp.js
@@ -5,23 +5,20 @@ export default () => {
   const templateSignUp = document.createElement('section');
   const viewSignUp = `
   <article class="register">
-  
-  <article class="left"></article>
-  <article class="right">
-
-  <a class="back" href=#/>◀</a>
-  <h1 class="letter">Regístrate</h1>
-  <input class="name" id="name" type="text" placeholder="Nombre Completo" required>
-  <input class="username" id="username" type="text" placeholder="Nombre de usuario" required>
-  <input class="email" id="email" type="email" placeholder="ichef@example.com" required>
-  <p class="errorMail" id= "errorMail" style="display: none"> Correo inválido. Por favor, revísalo nuevamente.</p>
-  <input class="password" id ="password" type ="password" placeholder = "*********" required>
-  <img src="img/google.png" class="google" id="btn-google"><br />
-  <p class="errorMail" id= "errorMailGoogle" style="display: none">No ingresaste con tu cuenta Google</p>
-  <button class="btn-correo" id="btn-correo">Registrarse</button>
-  <p id="alert-sendEmailVerification" style="display: none">Verifica tu correo en la bandeja.</p>
-  
-  </article>
+  <article class="leftR"></article>
+   <article class="right">
+    <a class="back" href=#/>↩</a>
+    <h1 class="letter">Regístrate</h1>
+    <input class="name" id="name" type="text" placeholder="Nombre Completo" required>
+    <input class="username" id="username" type="text" placeholder="Nombre de usuario" required>
+    <input class="email" id="email" type="email" placeholder="ichef@example.com" required>
+    <p class="errorMail" id= "errorMail" style="display: none"> Correo inválido. Por favor, revísalo nuevamente.</p>
+    <input class="password" id ="password" type ="password" placeholder = "*********" required>
+    <img src="img/google.png" class="google" id="btn-google"><br />
+    <p class="errorMail" id= "errorMailGoogle" style="display: none">No ingresaste con tu cuenta Google</p>
+    <button class="btn-correo" id="btn-correo">Registrarse</button>
+    <p id="alert-sendEmailVerification" style="display: none">Verifica tu correo en la bandeja.</p>
+   </article>
   </article>`;
 
   templateSignUp.classList.add('positionRegister');
--- src/view/signin.js
+++ src/view/signin.js
@@ -4,22 +4,21 @@ import { signInWithEmail, registerWithGoogle } from '../lib/view-controller.js';
 export default () => {
   const templateSignIn = document.createElement('section');
   const viewSignIn = `
-  <section id="color">
   <article class = "logIn">
   <article class="left"></article>
-  <article class="right">
-      <a class="back" href=#/>◀</a>
-      <h1 class="letter"> Inicia Sesión </h1>
-      <input class="email" id="email" type="email" placeholder="usuario@example.com">
-      <input class="password" id="password" type="password" placeholder="********">
-      <p class="errorMail" id= "errorMail" style="display: none"> Correo inválido. Por favor, revísalo nuevamente.</p>
-      <img src="img/google.png" class="google" id="btn-google">
-      <p class="errorMail" id= "errorMailGoogle" style="display: none">No ingresaste con tu cuenta Google</p>
-      <button class="btn-correo" id="btn-correo">Inicia Sesión</button>
-      <p id="alert-sendEmailVerification" style="display: none">Verifica tu correo en la bandeja.</p>
+   <article class="right">
+    <a class="back" href=#/>↩</a>
+    
+    <h1 class="letter"> Inicia Sesión </h1> <input class="email" id="email" type="email" placeholder="usuario@example.com">
+    <input class="password" id="password" type="password" placeholder="********">
+    <p class="errorMail" id= "errorMail" style="display: none"> Correo inválido. Por favor, revísalo nuevamente.</p>
+    <img src="img/google.png" class="google" id="btn-google">
+    <p class="errorMail" id= "errorMailGoogle" style="display: none">No ingresaste con tu cuenta Google</p>
+    <button class="btn-correo" id="btn-correo">Inicia Sesión</button>
+    <p id="alert-sendEmailVerification" style="display: none">Verifica tu correo en la bandeja.</p>
+   </article>
   </article>
-  </article>
-  </section>`;
+`;
 
   templateSignIn.classList.add('positionLogin');
   templateSignIn.innerHTML = viewSignIn;
